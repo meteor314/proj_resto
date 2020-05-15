b9af4cda907db3e43f1fdbf7a7e180d1e7b82e3a
meteor314 <56741348+meteor314@users.noreply.github.com>
2020-05-15 16:57:10 +0200

    Update proj_resto.sql

--- proj_resto.sql
+++ proj_resto.sql
@@ -59,10 +59,8 @@ CREATE TABLE `Commande` (
 --
 
 INSERT INTO `Commande` (`id`, `nom`, `email`, `isConfirm`, `nbCount`) VALUES
-(5, 'meteor314', 'meteor31415@gmail.com', 1, 7),
-(6, 'meteor314', 'meteor31415@gmail.com', 1, 3),
-(7, 'meteor314', 'meteor31415@gmail.com', 1, 3),
-(8, 'meteor314', 'meteor31415@gmail.com', 1, 2);
+
+(8, 'meteor314', '********@gmail.com', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -84,9 +82,7 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`id`, `pseudo`, `email`, `pwd`, `date_time_register`, `vide`) VALUES
-(1, 'meteor314', 'meteor31415@gmail.com', '$2y$11$N5uIUKIbuW2OAb5fYs6xAucTEJrLlbMslglwHcNg6mEqkmo7XsTHi', '2020-05-15', 0),
-(2, 'segfried', 'bastien.ponroy@cfautec.fr', '$2y$11$lv4xVc7z8BjnmNOWXyczm.BG3cqzdu1OEuDrSSyg129CfCMWFkPsa', '2020-05-15', 0),
-(10, 'siafall', 'faysel.menan@cfautec.fr', '$2y$11$dsl6d62GYBdmEF9b7Yj6vuRljWZ8Tobx5xWVLSj1p7VA15aeujzf.', '2020-05-15', 0);
+(1, 'meteor314', '******@gmail.com', '$2y$11$N5uIUKIbuW2OAb5fYs6xAucTEJrLlbMslglwHcNg6mEqkmo7XsTHi', '2020-05-15', 0);
 
 --
 -- Index pour les tables déchargées
